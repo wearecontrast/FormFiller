1d2ac02159b44a67965ef0da17d15a75f91193b0
mikeaag <mike.gatward@xenonsoft.co.uk>
2015-06-06 19:11:21 +0100

    Setting up main files

---
+++ src/FormFiller.js
@@ -0,0 +1,13 @@
+function FormFiller(){
+    console.log('Loading FormFiller');
+    
+    this.start = function(){
+        console.log('Starting...');
+        
+    };
+}
+
+
+
+var FormFiller = new FormFiller();
+FormFiller.start();
